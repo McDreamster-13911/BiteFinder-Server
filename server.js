481cebc9005cee61979b9e97a5ab27bfdb45bc12
Ibrahim <the.ibrahimkhan.mik3301@gmail.com>
2024-10-01 20:49:13 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.get('/api/restaurants', async (req, res) => {
         const response = await fetch(url, {
             headers: {
                 'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
             }
         });
