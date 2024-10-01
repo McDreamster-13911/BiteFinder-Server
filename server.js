975de9e3f4e463c9dd3c0ab293c64e47606d6a7f
Ibrahim <the.ibrahimkhan.mik3301@gmail.com>
2024-10-01 19:04:59 +0530

    Fixed server issue

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.options('*', cors(corsOptions));
+
 
 // For Restaurant API
 app.get('/api/restaurants', async (req, res) => {
